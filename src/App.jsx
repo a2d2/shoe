2194c2e18faaeccbdea10b10c69c1d0a2092a018
r2d2 <arturo.galvis@hotmail.com>
2023-07-04 05:56:07 -0500

    6

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,96 @@
-import { Stats, OrbitControls, Circle, Environment } from '@react-three/drei';
-import { Canvas } from '@react-three/fiber';
-import { useLoader } from '@react-three/fiber';
+import {
+  Stats,
+  OrbitControls,
+  Environment,
+  ContactShadows,
+} from '@react-three/drei';
+import { Canvas, useLoader } from '@react-three/fiber';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { useControls } from 'leva';
+import { Leva, useControls } from 'leva';
+
+function Model() {
+  const { scene } = useLoader(GLTFLoader, './models/libreta2.glb');
+  console.log(scene);
+  const {
+    x,
+    y,
+    z,
+    visible,
+    color,
+    // metalness,
+    // roughness,
+    // clearcoat,
+    // clearcoatRoughness,
+    // transmission,
+    // ior,
+    // thickness,
+  } = useControls('Suzanne', {
+    x: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
+    y: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
+    z: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
+    visible: true,
+    color: { value: '#ffbc85' },
+    // metalness: { value: 0, min: 0, max: 1.0, step: 0.01 },
+    // roughness: { value: 0, min: 0, max: 1.0, step: 0.01 },
+    // clearcoat: { value: 1, min: 0, max: 1.0, step: 0.01 },
+    // clearcoatRoughness: { value: 0, min: 0, max: 1.0, step: 0.01 },
+    // transmission: { value: 1.0, min: 0, max: 1.0, step: 0.01 },
+    // ior: { value: 1.74, min: 1, max: 5, step: 0.01 },
+    // thickness: { value: 3.12, min: 0, max: 5, step: 0.01 },
+  });
+
+  return (
+    <primitive
+      object={scene}
+      children-5-rotation={[x, y, z]}
+      children-5-visible={visible}
+      //children-5-0-0-0-1-4-material-color={color}
+      //children-5-material-metalness={metalness}
+      // children-0-material-roughness={roughness}
+      // children-0-material-clearcoat={clearcoat}
+      // children-0-material-clearcoatRoughness={clearcoatRoughness}
+      // children-0-material-transmission={transmission}
+      // children-0-material-ior={ior}
+      // children-0-material-thickness={thickness}
+    />
+  );
+}
 
 function Env() {
   const { height, radius, scale } = useControls('Ground', {
-    height: { value: 54, min: 0, max: 100, step: 1 },
-    radius: { value: 90, min: 0, max: 1000, step: 1 },
+    height: { value: 10, min: 0, max: 100, step: 1 },
+    radius: { value: 115, min: 0, max: 1000, step: 1 },
     scale: { value: 100, min: 0, max: 1000, step: 1 },
   });
   return (
     <Environment
-      files="./img/neon_photostudio_1k.hdr"
+      preset="sunset"
       background
-      // ground={{ height: height, radius: radius, scale: scale }}
+      // ground={{
+      //   height: height,
+      //   radius: radius,
+      //   scale: scale,
+      // }}
       blur={0.9}
     />
   );
 }
-export default function App() {
-  const gltf = useLoader(GLTFLoader, './models/libreta.glb');
-  console.log(gltf);
 
+export default function App() {
   return (
     <>
-      {/* <Canvas camera={{ position: [-0.5, 1, 2] }} shadows> */}
-      <Canvas camera={{ position: [-8, 5, 8], fov: 50 }}>
-        {/* <Environment preset="forest" background blur={0.9} /> */}
+      <Canvas camera={{ position: [-8, 5, 8] }}>
         <Env />
-        {/* <directionalLight position={[3.3, 1.0, 4.4]} intensity={1} castShadow /> */}
-        <primitive
-          object={gltf.scene}
-          position={[0, 0, 0]}
-          // children-0-castShadow
-        />
-        {/* <Circle args={[10]} rotation-x={-Math.PI / 2} receiveShadow>
-          <meshStandardMaterial />
-        </Circle> */}
-        {/* <OrbitControls target={[0, 1, 0]} autoRotate /> */}
+        <Model />
+        {/* <ContactShadows
+          scale={150}
+          position={[0.33, -0.33, 0.33]}
+          opacity={1.5}
+        /> */}
         <OrbitControls target={[0, 1, 0]} maxPolarAngle={Math.PI / 2} />
-        <axesHelper args={[5]} />
         <Stats />
       </Canvas>
+      <Leva collapsed />
     </>
   );
 }
