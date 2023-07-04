4539e3ec7e7ad773d1c14ca3e4978a03859f3bfc
r2d2 <arturo.galvis@hotmail.com>
2023-07-04 07:08:47 -0500

    7

--- src/App.jsx
+++ src/App.jsx
@@ -1,96 +1,43 @@
+import { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
 import {
   Stats,
   OrbitControls,
   Environment,
-  ContactShadows,
+  useGLTF,
+  Clone,
 } from '@react-three/drei';
-import { Canvas, useLoader } from '@react-three/fiber';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { Leva, useControls } from 'leva';
+import { useControls } from 'leva';
 
-function Model() {
-  const { scene } = useLoader(GLTFLoader, './models/libreta2.glb');
-  console.log(scene);
-  const {
-    x,
-    y,
-    z,
-    visible,
-    color,
-    // metalness,
-    // roughness,
-    // clearcoat,
-    // clearcoatRoughness,
-    // transmission,
-    // ior,
-    // thickness,
-  } = useControls('Suzanne', {
-    x: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
-    y: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
-    z: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
-    visible: true,
-    color: { value: '#ffbc85' },
-    // metalness: { value: 0, min: 0, max: 1.0, step: 0.01 },
-    // roughness: { value: 0, min: 0, max: 1.0, step: 0.01 },
-    // clearcoat: { value: 1, min: 0, max: 1.0, step: 0.01 },
-    // clearcoatRoughness: { value: 0, min: 0, max: 1.0, step: 0.01 },
-    // transmission: { value: 1.0, min: 0, max: 1.0, step: 0.01 },
-    // ior: { value: 1.74, min: 1, max: 5, step: 0.01 },
-    // thickness: { value: 3.12, min: 0, max: 5, step: 0.01 },
-  });
+const Models = [
+  { title: 'libreta4', url: './models/libreta4.glb' },
+  { title: 'libreta', url: './models/libreta.glb' },
+  { title: 'libreta2', url: './models/libreta2.glb' },
+];
 
-  return (
-    <primitive
-      object={scene}
-      children-5-rotation={[x, y, z]}
-      children-5-visible={visible}
-      //children-5-0-0-0-1-4-material-color={color}
-      //children-5-material-metalness={metalness}
-      // children-0-material-roughness={roughness}
-      // children-0-material-clearcoat={clearcoat}
-      // children-0-material-clearcoatRoughness={clearcoatRoughness}
-      // children-0-material-transmission={transmission}
-      // children-0-material-ior={ior}
-      // children-0-material-thickness={thickness}
-    />
-  );
-}
-
-function Env() {
-  const { height, radius, scale } = useControls('Ground', {
-    height: { value: 10, min: 0, max: 100, step: 1 },
-    radius: { value: 115, min: 0, max: 1000, step: 1 },
-    scale: { value: 100, min: 0, max: 1000, step: 1 },
-  });
-  return (
-    <Environment
-      preset="sunset"
-      background
-      // ground={{
-      //   height: height,
-      //   radius: radius,
-      //   scale: scale,
-      // }}
-      blur={0.9}
-    />
-  );
+function Model({ url }) {
+  const { scene } = useGLTF(url);
+  return <Clone object={scene} />;
 }
 
 export default function App() {
+  const { title } = useControls({
+    title: {
+      options: Models.map(({ title }) => title),
+    },
+  });
+
   return (
     <>
-      <Canvas camera={{ position: [-8, 5, 8] }}>
-        <Env />
-        <Model />
-        {/* <ContactShadows
-          scale={150}
-          position={[0.33, -0.33, 0.33]}
-          opacity={1.5}
-        /> */}
-        <OrbitControls target={[0, 1, 0]} maxPolarAngle={Math.PI / 2} />
+      <Canvas camera={{ position: [0, 0, -0.2], near: 0.025 }}>
+        <Environment files="./img/workshop_1k.hdr" background />
+        <Suspense>
+          <Model url={Models[Models.findIndex((m) => m.title === title)].url} />
+        </Suspense>
+        <OrbitControls autoRotate />
         <Stats />
       </Canvas>
-      <Leva collapsed />
+      <span id="info">The {title} is selected.</span>
     </>
   );
 }
